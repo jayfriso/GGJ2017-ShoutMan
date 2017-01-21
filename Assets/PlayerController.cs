20d92d1141abca9226ad4bea40e878b3b3ee94b1
Abdullah Abdulqader <UP2LLAA@GMAIL.COM>
2017-01-20 23:17:36 -0800

    MADE IT WORK BIIIIIIITCHHHH

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 [System.Serializable]
 public class Boundary {
 	public float xMin, xMax, zMin, zMax;
-
 }
 
 public class PlayerController : MonoBehaviour {
 	public Boundary boundary;
+	public GameObject startPos;
+	public GameObject endPos;
 
+	public float midPitch;
+	public float lowThreshhold;
+	public float speed;
+	private float currentSpeed;
+	public AudioController audioController;
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
 	}
 	
 	
-	void Update () {
-		
+	void Update ()
+	{
+		float currentFrequency = audioController.getPitch ();
+		if (currentFrequency > midPitch) {
+			currentSpeed = speed;
+			
+		} else if (currentFrequency < midPitch && currentFrequency > lowThreshhold) {
+			currentSpeed = -speed;
+		} else {
+			currentSpeed = 0;
+		}
+
+		transform.position = transform.position + new Vector3(0, currentSpeed * Time.fixedDeltaTime, 0);
+
 	}
 }
